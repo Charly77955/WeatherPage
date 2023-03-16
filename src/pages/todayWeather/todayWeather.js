750d2666959ed8aba0255f28dedac9774f739d54
Charly77955 <desantiscarlos95@gmail.com>
2023-03-16 18:50:39 -0300

    feat: add styles to today-weather

--- src/pages/todayWeather/todayWeather.js
+++ src/pages/todayWeather/todayWeather.js
@@ -1,4 +1,10 @@
 import React, {useState, useEffect} from 'react'
+import '../todayWeather/todayWeatherStyles.css'
+import Shower from '../../assets/weather-app-master/Shower.png'
+import findMe from '../../assets/weather-app-master/findMe.png'
+import location from '../../assets/weather-app-master/location.png'
+import circle from '../../assets/weather-app-master/circle.png'
+
 
 export default function TodayWeather() {
 
@@ -18,14 +24,32 @@ export default function TodayWeather() {
   }, [])
 
   return (
-    <div>
-      <h1>tiempo: {weather.degres}</h1>
+    <div className='container'>
+      <img src=''/>
+      <div className='components'>
+        <button className='button-search'><p className='button-search-font'>Search for places</p></button>
+        <div className='placement-icon'><img className='findMeIcon' src={findMe}/>
+        </div>
+      </div>
+      <div className='weather-icon'>
+        <img  className='weather-icon'src={Shower}/></div>
+      <div className='degres'>
+        <h1 className='degres-font'>{weather.degres}</h1></div>
+      <div className='weather-status'>
+        <h2 className='weather-status-font'>Shower</h2></div>
+      <div className='weather-highlights'>
+        <h5 className='today-font'>Today</h5>
+        <img className='circle-icon' src={circle}/>
+        <h5 className='date'>fri, 5 jun</h5>
+        <h5 className='city'>Helsinski</h5>
+        <img className='location-icon' src={location}/>
+      </div>
     </div>
   )
 }
 
 const data = {
-  degres: 15,
+  degres: '15°c',
   weather: 'Shower',
   city: 'Helsinski'
 }
